9774c55f0f87455db22a438a573aa6fa2aa806b5
Alessandro Rubino <aleruby97@hotmail.it>
2022-11-22 12:46:19 +0000

    Remove prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     tsconfigRootDir : __dirname, 
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
